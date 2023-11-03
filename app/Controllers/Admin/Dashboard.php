e47aaf1db0704d4fee142575cac6e1e1e2cfd767
Imamsaa <imamsafii2222022@gmai.com>
2023-11-03 13:02:41 +0700

    fix: melakukan templating pada tampilan admin agar bisa lebih dinamis nantinya

--- app/Controllers/Admin/Dashboard.php
+++ app/Controllers/Admin/Dashboard.php
@@ -8,6 +8,6 @@ class Dashboard extends BaseController
 {
     public function index(): string
     {
-        return view('admin/theme');
+        return view('admin/pages/dashboard');
     }
 }
---
+++ app/Views/admin/index.php
@@ -0,0 +1,7 @@
+<?= $this->include('admin/theme/head'); ?>
+<?= $this->include('admin/sections/profil'); ?>
+  <?= $this->include('admin/theme/nav'); ?>
+
+  <?= $this->include('admin/theme/aside'); ?>
+  <?= $this->renderSection('pages'); ?>
+  <?= $this->include('admin/theme/footer'); ?>
